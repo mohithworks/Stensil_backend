ec31ca663c8014d0f1fa7fe3fc1f18495ed91b5c
mohithworks <mohithworks2002@gmail.com>
2022-12-17 16:36:11 +0530

    CORS Changes 6

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+});
 
 app.get('/', function(req, res, next){
     res.send("Started");
