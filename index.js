f67686288147f9a0f7982596a6547c5cc16669a9
mohithworks <mohithworks2002@gmail.com>
2022-12-17 20:01:12 +0530

    CORS Changes 7

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language");
     res.header("Access-Control-Allow-credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
